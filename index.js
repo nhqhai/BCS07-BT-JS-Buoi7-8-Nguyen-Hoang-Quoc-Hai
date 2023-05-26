f42ead7272944a7fa8a47723441b6cb324af962c
nhqhai <nhqhai@gmail.com>
2023-05-26 09:41:47 +0700

    done 2 cau

--- index.js
+++ index.js
@@ -14,18 +14,31 @@ function hienThiSoNguyen(){
 document.querySelector(".btn-warning").onclick = hienThiSoNguyen;
 
 // -------------------------------------------
-// Tổng các số dương
-function tongSoDuong(){
-    var tongSoDuong = 0;
-    for (var i = 0; i < arrSoNguyen.length; i++) {
-        tongSoDuong += parseInt(arrSoNguyen[i]);
+// Nút bấm 1: Đếm các số dương
+function cacSoDuong(){
+    var soDuong = 0;
+    for (var i = 0; i < arrSoNguyen.length; i++){
+        if(arrSoNguyen[i] > 0)
+        soDuong ++;
     }
-    document.getElementById("hienThiTongSoDuong").innerHTML = tongSoDuong;
+    document.getElementById("hienThiCacSoDuong").innerHTML = soDuong;
 }
-document.querySelector(".btn-success").onclick = tongSoDuong;
+document.querySelector(".btn-success").onclick = cacSoDuong;
 
 // -------------------------------------------
-// Đếm các số dương
-function demSoDuong(){
+// Nút bấm 2: Tổng các số dương
+function tongCacSoDuong(){
+    var cacSoDuong = 0;
+    for (var i = 0; i < arrSoNguyen.length; i++) {
+        if(arrSoNguyen[i] > 0){
+            cacSoDuong += parseInt(arrSoNguyen[i]);
+        }
+    }
+    document.getElementById("hienThiTongSoDuong").innerHTML = cacSoDuong;
+}
+document.querySelector(".btn-danger").onclick = tongCacSoDuong;
+
+// Nút bấm 3:
+function soNhoNhat(){
 
 }
